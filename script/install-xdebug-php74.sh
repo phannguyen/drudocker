c1300659717158f7b1a5d302894430f9e64a174a
Phan Nguyen <nguyen.p@intergreat.com>
2024-02-28 16:07:23 +0700

    Update structure

---
+++ script/install-xdebug-php74.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+XDEBUG_INI="/etc/php/7.4/apache2/conf.d/20-xdebug.ini"
+
+add_xdebug_config() {
+  CONFIG=$1
+  FILE=$2
+
+  if ! grep -qF -- "$CONFIG" "$FILE"; then
+    echo "$CONFIG" >> "$FILE"
+  fi
+}
+
+# Install Xdebug
+docker-compose exec -T php74 apt-get update
+docker-compose exec -T php74 apt-get install -y php7.4-xdebug
+
+# Add Xdebug configuration
+docker-compose exec -T php74 bash -c "$(declare -f add_xdebug_config); add_xdebug_config 'xdebug.mode=debug' $XDEBUG_INI"
+docker-compose exec -T php74 bash -c "$(declare -f add_xdebug_config); add_xdebug_config 'xdebug.start_with_request=yes' $XDEBUG_INI"
+docker-compose exec -T php74 bash -c "$(declare -f add_xdebug_config); add_xdebug_config 'xdebug.client_host=host.docker.internal' $XDEBUG_INI"
+docker-compose exec -T php74 bash -c "$(declare -f add_xdebug_config); add_xdebug_config 'xdebug.client_port=9003' $XDEBUG_INI"
+docker-compose exec -T php74 bash -c "$(declare -f add_xdebug_config); add_xdebug_config 'xdebug.log=/var/www/html/xdebug.log' $XDEBUG_INI"
+
+# Restart PHP-FPM
+docker-compose exec -T php74 service apache2 reload
