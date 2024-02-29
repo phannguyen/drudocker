6d00ae19eaa96655c618269de7ebe4c5ab0b9396
Phan Nguyen <nguyen.p@intergreat.com>
2024-02-29 09:49:52 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,20 @@
-docker-compose up -d && docker exec -it dru-server-php7 bash
+#!/bin/bash
+
+PHP_VERSION=${2:-php74}
+
+if [ "$1" == "-i" ]; then
+  PHP_CONTAINER="dru-server-${PHP_VERSION}"
+else
+  PHP_CONTAINER="dru-server-php74"
+fi
+
+CONTAINER_STATUS=$(docker inspect --format='{{.State.Status}}' $PHP_CONTAINER)
+
+if [ "$CONTAINER_STATUS" == "running" ]; then
+  docker exec -it $PHP_CONTAINER bash
+else
+  docker-compose up -d && docker exec -it $PHP_CONTAINER bash
+fi
+
+
+
